92019ea1596dd08969969358b47d9ecdf0080112
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-05-31 22:38:01 +0600

    added food item api

--- index.js
+++ index.js
@@ -44,38 +44,62 @@ async function run() {
   })
   // middlewares//
   const verifyToken = (req, res, next) =>{
-    console.log( 'inside verify token', req.headers.authorization);
     if(!req.headers.authorization){
-      return res.status(401).send('forbidden access');
+      return res.status(401).send('unauthorized access');
     }
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
       if(err){
-        return res.status(401).send({message: 'forbidden access'});
+        return res.status(401).send({message: 'unauthorized access'});
       }
       req.decoded = decoded;
       next();
     })
 
   }
+  // verify token after verify token
+  const verifyAdmin = async (req, res, next) =>{
+    const email = req.decoded?.email;
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.Role === 'admin';
+    if(!isAdmin){
+      return res.status(403).send({message: 'forbidden access'})
+    };
+    next();
+  }
    //users related api//
-   app.get('/users', verifyToken, async(req, res) =>{
+   app.get('/users', verifyToken, verifyAdmin, async(req, res) =>{
     const users = req.body;
     const result = await userCollection.find(users).toArray()
     res.send(result)
+   })
+   app.get('/user/admin/:email', verifyToken, async(req, res) =>{
+    const email = req.params.email;
+    if(email !== req.decoded.email){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if(user){
+     admin = user?.Role === 'admin'
+    }
+    res.send({admin})
+
    })
   app.post('/user', async(req, res) =>{
     const user = req.body;
     const result = await userCollection.insertOne(user);
     res.send(result)   
   })
-  app.delete('/users/:id', async(req, res) =>{
+  app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) =>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)};
     const result = await userCollection.deleteOne(query)
     res.send(result)
   })
-  app.patch('/users/admin/:id', async(req, res) =>{
+  app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) =>{
     const id = req.params.id;
     const filter = {_id: new ObjectId(id)}
     const updatedDoc = {
@@ -88,6 +112,11 @@ async function run() {
   })
   
   // menu related api //
+  app.post('/menu', verifyToken, verifyAdmin, async (req, res) =>{
+    const item = req.body;
+    const result = await menuCollection.insertOne(item);
+    res.send(result);
+  })
    app.get('/menu', async(req, res) =>{
     const result = await menuCollection.find().toArray();
     res.send(result)
